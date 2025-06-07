0c861c25bc29809f5248c751b9c708f705282d5e
millerkiller96 <100490150+millerkiller96@users.noreply.github.com>
2025-06-07 11:19:05 -0400

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,8 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  // Set the base path for GitHub Pages
+  base: '/PluginFactory_PrivacyPolicy/',
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
